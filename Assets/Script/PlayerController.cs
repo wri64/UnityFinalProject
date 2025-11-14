c9bf63a94bf8869464ae1654e9d246f957409cdb
imrxna <134999411+imrxna@users.noreply.github.com>
2025-11-14 12:01:11 +0700

    walkPlayer

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+	[Header("Horizontal Movement Setting")]
+	[SerializeField] private Rigidbody2D rb;
 
-    private float walkSpeed = 1;
+
+	[SerializeField] private float walkSpeed = 1;
 
     private float xAxis;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
